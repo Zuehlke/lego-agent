af30073966be6ab57533cbbece80a58cbd34d017
Silvan Melchior <silvan.melchior@zuehlke.com>
2025-03-26 11:07:54 +0100

    init frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Lego Agent",
+  description: "Control your Lego Mindstorms EV3 robot with a web interface.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
